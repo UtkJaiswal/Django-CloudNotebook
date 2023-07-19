455d54df367cb530b22bbb1f21128fb830a0b722
utkarsh <utkjaiswal58@gmail.com>
2023-07-20 01:24:52 +0530

    Initialez a serializer and model

---
+++ notes/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from notes.models import Note, LANGUAGE_CHOICES, STYLE_CHOICES
+
+
+class NoteSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    desc = serializers.CharField(required=False, allow_blank=True, max_length=100)
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        return Note.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Snippet` instance, given the validated data.
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.desc = validated_data.get('code', instance.desc)
+        instance.save()
+        return instance
---
+++ notes/urls.py
@@ -0,0 +1,8 @@
+from .views import *
+from django.urls import path, include
+
+
+
+urlpatterns = [
+    # path('/', Notes.as_view(), name='note')
+]
